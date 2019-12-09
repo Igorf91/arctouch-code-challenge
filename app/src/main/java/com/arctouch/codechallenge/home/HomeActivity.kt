6a98123e4fa34f83d3a2865f809fcee57f1e1a57
igor <igor.fds91@hotmail.com>
2019-12-09 00:52:20 -0300

    Create a temporary solution to endless scroll view

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -4,16 +4,21 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.api.RetrofitFactory.Companion.getApi
 import com.arctouch.codechallenge.api.TmdbApi
+import com.arctouch.codechallenge.util.EndlessRecyclerViewScrollListener
 import kotlinx.android.synthetic.main.home_activity.progressBar
 import kotlinx.android.synthetic.main.home_activity.recyclerView
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var viewModel: HomeViewModel
+    private lateinit var adapter: HomeAdapter
+    private lateinit var linear: LinearLayoutManager
 
     private val api : TmdbApi by lazy {
         getApi()
@@ -23,7 +28,27 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
+        setupRecyclerView()
         setupViewModel()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = HomeAdapter(arrayListOf())
+        linear = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = linear
+
+        val scrollListener: EndlessRecyclerViewScrollListener =
+            object : EndlessRecyclerViewScrollListener(linear) {
+                override fun onLoadMore(
+                    page: Int,
+                    totalItemsCount: Int,
+                    view: RecyclerView?
+                ) {
+                    viewModel.fetchUpcomingMovies((page + 1).toLong())
+                }
+            }
+        recyclerView.addOnScrollListener(scrollListener)
     }
 
     private fun setupViewModel() {
@@ -34,7 +59,7 @@ class HomeActivity : AppCompatActivity() {
         viewModel.loadGenres()
 
         viewModel.upcomingMovies.observe(this, Observer {
-            recyclerView.adapter = HomeAdapter(it.orEmpty())
+            adapter.postValue(it.orEmpty())
             progressBar.visibility = View.GONE
         })
 
